8a1b5c5996c65cff5a548ab47baaa5dacf3562d1
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-09-27 07:23:08 -0300

    Finaliza primeiro endpoint

--- src/modules/usuario/usuario-controller/usuario-controller.controller.ts
+++ src/modules/usuario/usuario-controller/usuario-controller.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Get } from '@nestjs/common';
 import { UsuarioService } from '../usuario-service/usuario-service.service';
 import { Usuario } from '../usuario.entity';
 
-@Controller('usuario-controller')
+@Controller('usuarios')
 export class UsuarioController {
 
     constructor(private readonly usuarioService: UsuarioService){}
 
-    @Get()
+    @Get('/all')
     async findAll(): Promise<Usuario[]> {
         return this.usuarioService.findAll();
     }
